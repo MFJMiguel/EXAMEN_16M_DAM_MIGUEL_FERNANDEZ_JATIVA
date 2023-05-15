2e2bc4494d0ee8bb0dd3b4a66e832ec81519f7c2
migue <migue@DESKTOP-MGITEER>
2023-05-15 09:18:56 +0200

    Subir.

---
+++ src/Pack1/Primo.java
@@ -0,0 +1,23 @@
+package Pack1;
+
+import java.util.Scanner;
+
+public class Primo {
+
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		int num2 = 0;
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez.");
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		Scanner ent = new Scanner(System.in);
+		num = ent.nextInt();
+		do {
+			if(num1/num2) {
+				System.out);
+			}
+		}while(numero);
+			
+	}
+	
+}
